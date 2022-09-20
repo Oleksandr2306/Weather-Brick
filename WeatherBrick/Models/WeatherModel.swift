f7ededa6ca5b96f990334668c429a019f08d2504
Oleksandr2306 <sashamelnik2306@gmail.com>
2022-09-20 12:19:18 +0300

    models added

---
+++ WeatherBrick/Models/WeatherModel.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherModel.swift
+//  WeatherBrick
+//
+//  Created by Oleksandr Melnyk on 12.09.2022.
+//  Copyright © 2022 VAndrJ. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let name: String
+    let country: String
+    let temp: Double
+    let condition: String
+    let id: Int
+}
